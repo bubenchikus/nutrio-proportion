51d2336b21c7ae77021c73b0048b129f81cbb220
bubenchikus <dozythoth@yandex.ru>
2023-07-03 14:07:13 +0300

    Forced desktop view until adaptive css is implemented. Changed logo font size.

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -6,6 +6,7 @@ export default function Document() {
       <title>Nutrio-proportion</title>
       <Head>
         <meta keywords={"nutrio-proportion, nutrition, foods-rating"}></meta>
+        <meta name="viewport" content="width=1024" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
